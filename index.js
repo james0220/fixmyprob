dc0acb5f5a3e8dc62d43facefd53fb30fce462d9
Emily Chen <Emily@Emilys-MacBook-Pro-5.local>
2017-03-26 15:34:39 -0400

    add dependencies and attempt to render view

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 'use strict';
 
 const Hapi = require('hapi');
-var mysql      = require('mysql');
+var mysql = require('mysql');
+const Hoek = require('hoek');
 
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'users'
-});
+
+// WORK IN PROGRESS
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'users'
+// });
 
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 
-server.connection({ 
-    host: process.env.HOST || '0.0.0.0', 
+server.connection({
+    host: process.env.HOST || '0.0.0.0',
     port: process.env.PORT || 8000
+});
+
+// Render templates with vision and handlebars
+server.register(require('vision'), (err) => {
+
+    Hoek.assert(!err, err);
+
+    server.views({
+        engines: {
+            html: require('handlebars'),
+            compileMode: 'async' // global setting
+        },
+        relativeTo: __dirname,
+        path: './views'
+    });
 });
 
 
 //STILL WORK IN PROGRESS; used code from exam for starter
-connection.connect(); //connect to mysql
+// connection.connect(); //connect to mysql
+
 // Add the route
 
+// WORK IN PROGRESS FOR SERVER
+// server.route({
+//     method: 'GET',
+//     path:'/',
+//     handler: function (request, reply) {
+//         connection.query('SELECT * FROM People', function (error, results, fields) {
+//             if (error) throw error;
+//             reply('The name is: ' + results[0].Name);
+//             });
+//     }
+// });
+
+
+// Add the route
 server.route({
     method: 'GET',
-    path:'/', 
+    path: '/',
     handler: function (request, reply) {
-        connection.query('SELECT * FROM People', function (error, results, fields) {
-            if (error) throw error;
-            reply('The name is: ' + results[0].Name);
-            });
+        reply.view('index', { title: 'Home page' });
     }
 });
 
